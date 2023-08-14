9f5bce34880dfce8aa26944fae6e9cb580cf6eea
Darkatov <atoperez369@gmail.com>
2023-08-14 20:43:04 +0200

    add: custom basics visual

--- atoperezdev/src/components/Contact.js
+++ atoperezdev/src/components/Contact.js
@@ -3,9 +3,12 @@ import React from 'react';
 function Contact() {
   return (
     <section className="contact">
-      <h2>Contacto</h2>
-      <p>Email: tu@email.com</p>
-      <p>Teléfono: +123456789</p>
+      <h2>Redes</h2>
+      <p>Github: github.com/DARKATOV </p>
+      <p>CodeWars: linkedin.com/in/ato-perez </p>
+      <p>Stack Overflow: stackoverflow.com/users/211472 </p>
+      <p>Google Develover: https://developers.google.com/profile/u/darkatov </p>
+
     </section>
   );
 }
