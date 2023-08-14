756ec557e5a9a33353bb2d1cc8bcae13e877b167
Darkatov <atoperez369@gmail.com>
2023-08-14 20:03:16 +0200

    add: test

--- atoperezdev/src/components/Projects.js
+++ atoperezdev/src/components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Projects() {
   return (
     <section className="projects">
-      <h2>Proyectos</h2>
+      <h2>Proyecto</h2>
       {/* Aquí puedes agregar una lista de proyectos */}
     </section>
   );
